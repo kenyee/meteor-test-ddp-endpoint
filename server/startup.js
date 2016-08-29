124616ff611258190ffa37b7fa3a521fd1a64779
Ken Yee <kenyee@debtest.keysolutions.com>
2016-08-28 22:05:50 -0400

    update to Meteor 1.4.1

--- server/startup.js
+++ server/startup.js
@@ -2,6 +2,18 @@
 
 Meteor.startup(function () {
   // code to run on server at startup
+  if (Meteor.users.find().count() === 0) {
+    Accounts.createUser({
+        username: 'test@test.com',
+        email: 'test@test.com',
+        password: 'password',
+        profile: {
+            first_name: 'test',
+            last_name: 'user',
+            company: 'Test Inc.',
+        }
+    });
+  }
 });
 
 // only one collection to publish
